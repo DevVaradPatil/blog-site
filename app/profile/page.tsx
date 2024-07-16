00ac8623ff86874ef76beb3703023214262ad347
DevVaradPatil <varadapatil123@gmail.com>
2024-07-16 19:37:04 +0530

    profile rendered dynamically without ssr

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,21 @@
 "use client";
-import HomeBar from "@/components/homebar";
-import UserProfile from "@/components/user-profile";
+
+import dynamic from 'next/dynamic';
 import { useCurrentUser } from "@/hooks/use-current-user";
 
+// Dynamically import components with SSR disabled
+const DynamicHomeBar = dynamic(() => import('@/components/homebar'), { ssr: false });
+const DynamicUserProfile = dynamic(() => import('@/components/user-profile'), { ssr: false });
+
 const Profile = () => {
-    const user = useCurrentUser();
+  const user = useCurrentUser();
+
   return (
     <div className="w-full min-h-screen items-start justify-center flex bg-slate-100">
-        <HomeBar/>
-        {user && <UserProfile user={user} />}
+      <DynamicHomeBar />
+      {user && <DynamicUserProfile user={user} />}
     </div>
-  )
-} 
+  );
+};
 
-export default Profile
+export default Profile;
